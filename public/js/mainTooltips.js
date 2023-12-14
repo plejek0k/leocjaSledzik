3a8fafd3d514f9834a110c25b333cbe5fb2c1deb
Plejek <102672931+plejek0k@users.noreply.github.com>
2023-12-14 22:44:00 +0100

    [0.6] adding tippy.js + basic tooltips

---
+++ public/js/mainTooltips.js
@@ -0,0 +1,27 @@
+const raz = document.getElementById("raz");
+const dwa = document.getElementById("dwa");
+
+const leocja = document.getElementById("leocja");
+const edel = document.getElementById("edel");
+const vox = document.getElementById("vox");
+
+tippy(raz, {
+  content: "Kamery na żywo z Boca Chica",
+  placement: "bottom",
+});
+tippy(dwa, {
+  content: "Bazy wiedzy (TBD)",
+  placement: "bottom",
+});
+tippy(leocja, {
+  content: "Palatynat Leocji",
+  placement: "bottom",
+});
+tippy(edel, {
+  content: "Kotlina Edelweiss",
+  placement: "bottom",
+});
+tippy(vox, {
+  content: "Królestwo Voxlandu",
+  placement: "bottom",
+});
--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ function getStatusAbbreviation(status) {
     "To Be Determined": "DO USTALENIA",
     "Launch was a Partial Failure": "CZĘŚCIOWA PORAŻKA",
     "Launch Successful": "SUKCES",
-    "Launch Failure": "PORAŻKA"
+    "Launch Failure": "PORAŻKA",
   };
 
   return statusMap[status] || status;
@@ -19,7 +19,7 @@ function translateLaunch(locationName) {
     "Taiyuan Satellite Launch Center": "Taiyuan Launch Center",
     "Wenchang Space Launch Site": "Wenchang Launch Site",
     "Baikonur Cosmodrome": "Baikonur",
-    "SpaceX Space Launch Facility": "Boca Chica"
+    "SpaceX Space Launch Facility": "Boca Chica",
   };
   return launchPoprawka[locationName] || locationName;
 }
@@ -31,7 +31,7 @@ function poprawaMisji(missionName) {
 
   const regex = /Dragon CRS-2 SpX-(\d+)/;
   const match = missionName.match(regex);
-  
+
   if (match) {
     return "CRS-" + match[1];
   } else {
@@ -43,7 +43,7 @@ function skrotAgencji(agencyInfo) {
   const poprawkaAgencji = {
     "China Aerospace Science and Technology Corporation": "CASC",
     "Indian Space Research Organization": "ISRO",
-    "Russian Federal Space Agency (ROSCOSMOS)": "ROSCOSMOS"
+    "Russian Federal Space Agency (ROSCOSMOS)": "ROSCOSMOS",
   };
   return poprawkaAgencji[agencyInfo] || agencyInfo;
 }
@@ -51,7 +51,8 @@ function skrotAgencji(agencyInfo) {
 function brakOrbity(orbitInfo) {
   const brakOrbityPoprawka = {
     "N/A": "BRAK INFORMACJI",
-    "Sub": "LOT SUBORBITALNY"
+    "Sub": "LOT SUBORBITALNY",
+    "Elliptical": "ELIPTYCZNA"
   };
   return brakOrbityPoprawka[orbitInfo] || orbitInfo;
 }
@@ -73,13 +74,13 @@ function fetchData() {
   const url = "https://ll.thespacedevs.com/2.2.0/launch/upcoming/?limit=10";
 
   fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    localStorage.setItem("cachedData", JSON.stringify(data));
-    localStorage.setItem("cachedTimestamp", new Date().getTime());
+    .then((res) => res.json())
+    .then((data) => {
+      localStorage.setItem("cachedData", JSON.stringify(data));
+      localStorage.setItem("cachedTimestamp", new Date().getTime());
 
-    displayLaunchData(data.results);
-  });
+      displayLaunchData(data.results);
+    });
 }
 
 function displayLaunchData(results) {
@@ -99,12 +100,35 @@ function displayLaunchData(results) {
 
       const rocketName = result.rocket.configuration.name;
 
-      const missionName = poprawaMisji(result.mission.name.replace(/\(([^)]+)\)/g,''));
-      
+      const missionName = poprawaMisji(
+        result.mission.name.replace(/\(([^)]+)\)/g, "")
+      );
 
       const rocketNameElement = document.createElement("p");
       rocketNameElement.textContent = `${rocketName}`;
       rocketNameElement.className = "nazwaRakiety";
+      rocketNameElement.dataset.tippyContent = getTooltipContentForRocket(rocketName);
+      
+      tippy(rocketNameElement, {
+        content: rocketNameElement.dataset.tippyContent,
+        placement: "top",
+        allowHTML: true
+      });
+      
+      function getTooltipContentForRocket(rocketName) {
+        const tooltipRocket = {
+          "Falcon 9": "Rakieta wielokrotnego użytku wyprodukowana firmę SpaceX będąca pierwszą komercyjną rakietą, która wyniosła człowieka na orbitę i obecnie jest jedyną amerykańską rakietą, która może to zrobić",
+          "Long March 5": "Chińska ciężka rakieta nośna opracowana przez Chińską Akademię Technologii Pojazdów Startowych, będąca najpotężniejszą rakietą z rodziny rakiet Long March",
+          "Hyperbola-1": "Chińska rakieta składająca się z czterech stopni, które są zasilane wyłącznie paliwem stałym, bazująca na pociskach balistycznych DF-11 lub DF-15",
+          "Electron": "Nowozelandzka dwustopniowa rakieta opracowana przez firmę Rocket Lab będąca pierwszą rakietą zasilaną przez silnik z pompą elektryczną",
+          "Soyuz 2.1b/Fregat-M": "Zmodernizowana wersja radzieckiej trzystopniowej rakiety Sojuz z silnikiem RD-0124, ze zwiększoną ładownością do 8,2 ton",
+          "New Shepard": "Suborbitalna rakieta wielokrotnego użytku opracowany z myślą o turystyce kosmicznej przez firmę Blue Origin, nazwany na cześć Alana Sheparda, który jako pierwszy Amerykanin poleciał w kosmos",
+          "Falcon Heavy": "Superciężka rakieta nośna, która została zaprojektowana przez amerykańską firmę SpaceX",
+          "PSLV-DL": "Jeden z wariantów indyjskiej rakiety PSLV, który ma tylko dwa boostery i jest w stanie wystrzelić ładunek na orbitę synchroniczną ze Słońcem",
+          "Long March 2": "Rakieta nośna obsługiwana przez Chińską Republikę Ludową, przy czym sam rozwój rakiety odbywa się dzięki Chińskiej Akademii Technologii Pojazdów Startowych"
+        };
+        return tooltipRocket[rocketName] || "Oops... Chyba nie ustawiłem tutaj opisu";
+      }
 
       const missionNameElement = document.createElement("p");
       missionNameElement.textContent = `${missionName}`;
@@ -119,7 +143,7 @@ function displayLaunchData(results) {
       if (statusElement.innerHTML === "SUKCES") {
         statusElement.className = "udany";
         missionNameElement.className = "poMisji";
-      } 
+      }
       if (statusElement.innerHTML === "PORAŻKA") {
         statusElement.className = "nieUdany";
         missionNameElement.className = "poMisji";
@@ -134,9 +158,11 @@ function displayLaunchData(results) {
         statusElement.className = "potwierdzone";
       }
 
-      const locationName = translateLaunch(result.pad.location.name.split(",")[0]);
+      const locationName = translateLaunch(
+        result.pad.location.name.split(",")[0]
+      );
       const locationElement = document.createElement("p");
-      locationElement.innerHTML = `${locationName}`; 
+      locationElement.innerHTML = `${locationName}`;
       locationElement.className = "lokalizacja";
 
       const countdownElement = document.createElement("div");
@@ -157,9 +183,47 @@ function displayLaunchData(results) {
       const agencyInfoElement = document.createElement("p");
       agencyInfoElement.textContent = `${agencyInfo}`;
       agencyInfoElement.className = "agencyInfo";
+      agencyInfoElement.dataset.tippyContent = getTooltipContentForAgency(agencyInfo);
+
+      tippy(agencyInfoElement, {
+        content: agencyInfoElement.dataset.tippyContent,
+        placement: "top",
+        allowHTML: true
+      });
+      
+      function getTooltipContentForAgency(agencyInfo) {
+        const tooltipAgency = {
+          "SpaceX": "Amerykańska firma świadcząca usługi transportu kosmicznego, która została założona w 2002 roku przez Elona Muska w celu obniżenia kosztów transportu kosmicznego",
+          "iSpace": "Założona w październiku 2016 roku chińska prywatna firma zajmująca się rozwojem i wystrzeliwaniem rakiet w przestrzeń kosmiczną",
+          "ROSCOSMOS": "Rządowa agencja odpowiedzialną za program kosmiczny Federacji Rosyjskiej i ogólne rosyjskie badania lotnicze",
+          "Blue Origin": "Amerykański prywatny producent lotniczy i firma świadcząca usługi lotów kosmicznych, która została założona przez Jeffa Bezosa",
+          "ISRO": "Indyjska agencją kosmiczna, której główną wizją jest wykorzystywanie technologii kosmicznej do rozwoju Indii przy jednoczesnym prowadzeniu badań naukowych",
+          "CASC": "Główny wykonawca chińskiego programu kosmicznego, który został oficjalnie powołany w lipcu 1999 roku przez rząd Chińskiej Republiki Ludowej",
+          "Rocket Lab": "Amerykańska firma z branży lotniczej i kosmicznej, która posiada spółkę zależną w Nowej Zelandii, która opracowuje komercyjne rakiety nośne",
+        };
+        return tooltipAgency[agencyInfo] || "Oops... Chyba nie ustawiłem tutaj opisu";
+      }
 
       const wynikMisji = document.createElement("div");
       wynikMisji.className = "wynikMisji";
+      statusElement.dataset.tippyContent = getTooltipContentForResult(status);
+
+      tippy(statusElement, {
+        content: statusElement.dataset.tippyContent,
+        placement: "top",
+        allowHTML: true
+      });
+      
+      function getTooltipContentForResult(statusElement) {
+        const tooltipResult = {
+          "SUKCES": "Pomyślnie wyniesiono ładunek na docelową orbitę",
+          "DATA POTWIERDZONA": "Data została potwierdzona przez oficjalne źródła",
+          "DO POTWIERDZENIA": "Oczekiwanie na oficjalne potwierdzenie daty, która jest mniej więcej znana",
+          "DO USTALENIA": "Data jest bardzo przybliżona i jest oparta na niewiarygodnych źródłach"
+        };
+        return tooltipResult[statusElement] || "Oops... Chyba nie ustawiłem tutaj opisu";
+      }
+
 
       dupaElement.appendChild(missionNameElement);
       launchElement.appendChild(dupaElement);
@@ -171,7 +235,6 @@ function displayLaunchData(results) {
       info.appendChild(locationElement);
       info2.appendChild(orbitInfoElement);
       info2.appendChild(agencyInfoElement);
-
 
       if (result.image) {
         dupaElement.style.backgroundImage = `url(${result.image})`;
@@ -198,24 +261,27 @@ function displayLaunchData(results) {
         const now = new Date().getTime();
         const launchTime = new Date(result.net).getTime();
         const timeRemaining = launchTime - now;
-        
 
         if (timeRemaining <= 0) {
           countdownElement.innerHTML = "W TRAKCIE";
           countdownElement.className = "wTrakcie";
-        } 
+        }
         if (timeRemaining < 4) {
-          countdownElement.style.display = 'none';
+          countdownElement.style.display = "none";
         } else {
           const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-          const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+          const hours = Math.floor(
+            (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+          );
+          const minutes = Math.floor(
+            (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+          );
           const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-          const formattedDays = String(days).padStart(2, '0');
-          const formattedHours = String(hours).padStart(2, '0');
-          const formattedMinutes = String(minutes).padStart(2, '0');
-          const formattedSeconds = String(seconds).padStart(2, '0');
+          const formattedDays = String(days).padStart(2, "0");
+          const formattedHours = String(hours).padStart(2, "0");
+          const formattedMinutes = String(minutes).padStart(2, "0");
+          const formattedSeconds = String(seconds).padStart(2, "0");
 
           let countdownText = `<div id="czasElement">          
           <div id="elementCzasuHolder">
