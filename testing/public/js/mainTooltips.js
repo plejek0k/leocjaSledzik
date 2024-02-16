2137caa06def215b18d763e2d0f2aec4b974bbc2
Plejek <102672931+plejek0k@users.noreply.github.com>
2024-02-16 21:05:26 +0100

    [0.7.1] A major update of the entire layout of Śledzik

---
+++ testing/public/js/mainTooltips.js
@@ -0,0 +1,52 @@
+const unu = document.getElementById("unu");
+const du = document.getElementById("du");
+const tri = document.getElementById("tri");
+const kvar = document.getElementById("kvar");
+const kvin = document.getElementById("kvin");
+
+const leocja = document.getElementById("leocja");
+const edel = document.getElementById("edel");
+const vox = document.getElementById("vox");
+const nor = document.getElementById("nor");
+
+
+//tooltipy do ikonek
+tippy(unu, {
+  content: "Kamery na żywo z Boca Chica",
+  placement: "bottom",
+});
+tippy(du, {
+  content: "Bazy wiedzy (TBD)",
+  placement: "bottom",
+});
+tippy(tri, {
+  content: "Mikronacyjne starty (TBD)",
+  placement: "bottom",
+});
+tippy(kvar, {
+  content: "Statystyki (TBD)",
+  placement: "bottom",
+});
+tippy(kvin, {
+  content: "Repozytorium GitHub",
+  placement: "bottom",
+});
+
+//kraje
+tippy(leocja, {
+  content: "Palatynat Leocji",
+  placement: "bottom",
+});
+tippy(edel, {
+  content: "Kotlina Edelweiss",
+  placement: "bottom",
+});
+tippy(vox, {
+  content: "Królestwo Voxlandu",
+  placement: "bottom",
+});
+tippy(nor, {
+  content: "Federacja Nordacka",
+  placement: "bottom",
+});
+
